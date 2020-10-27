2525ad916868a23f5a5d3cc221762b92a89270d9
Anton <anro28yt@gmail.com>
2020-10-27 23:33:11 +0200

    replais

--- script/script.js
+++ script/script.js
@@ -1,118 +1,72 @@
 $(document).ready(function () {
   $('.slider').slick({
-    arrows: false,
-    dots: true,
-    speed: 1000,
-    autoplay: true,
-    responsive: [{
-      breakpoint: 1024,
-      settings: {
-        arrow: false,
-        infinite: true,
-        dots: true,
-      }
-    },
-    {
-      breakpoint: 767,
-      settings: {
-        arrows: false,
-        infinite: true,
-        dots: true,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        arrows: false,
-        infinite: true,
-        dots: true,
-      }
-    }
-  ]
-  });
-});
-
-$(document).ready(function () {
-  $('.products').slick({
-    arrow: true,
+    arrow: false,
     infinite: false,
     dots: false,
-    speed: 300,
-    autoplay: true,
+    autoplay: false,
     slidesToShow: 4,
     slidesToScroll: 1,
     responsive: [{
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
+          slidesToShow: 4,
           slidesToScroll: 1,
-          infinite: true,
+          infinite: false,
           dots: false,
         }
       },
       {
         breakpoint: 767,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
-          dots: false,
+          infinite: true,
+          dots: true,
         }
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 2,
+          slidesToShow: 1,
           slidesToScroll: 1,
-          dots: false,
+          infinite: true,
+          dots: true,
         }
       }
     ]
   });
 });
 
+const customSelectors = $('.customSelect');
+const slideSpeed = 200;
 
-
-
-
-$(document).ready(function () {
-  $('.slick-footer').slick({
-    slidesToShow: 6,
-    slidesToScroll: 1,
-    arrows: false,
-    infinite: false,
-    speed: 300,
-    responsive: [{
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 5,
-          slidesToScroll: 1,
-          arrows: false,
-          infinite: true,
-          speed: 300,
-
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 2,
-          arrows: false,
-          infinite: true,
-          speed: 300,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          arrows: false,
-          infinite: true,
-          speed: 300,
-        }
-      }
-    ]
+function closeAll(el) {
+  $.each(customSelectors, function () {
+    const ul = this.querySelector('ul');
+    const isVisible = $(ul).is(':visible');
+    const isPassedElement = el && el[0] === ul;
+    if (isVisible && !isPassedElement) {
+      $(ul).slideUp(slideSpeed);
+    }
   });
+}
+
+$('html').on('click', function(e) {
+  if($(e.target).parents('.customSelect').length == 0) {
+    /* Hide dropdown here */
+    closeAll();
+  }
+});
+
+$(customSelectors).on('click', function () {
+  const ul = this.querySelector('ul');
+  $(ul).slideToggle(slideSpeed);
+  closeAll($(ul));
+});
+
+$(customSelectors).on('click', 'li', function () {
+  const parent = $(this).parent().siblings('.selected').find('.selected');
+  parent[0].innerHTML = this.innerHTML;
+  closeAll($(this).parent());
 });
 
