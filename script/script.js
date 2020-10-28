4a35a6fe4700fafefd88904d2b0e9a93139a4937
deeartrama88 <deeartrama88@gmail.com>
2020-10-28 17:36:44 +0200

    testing

--- script/script.js
+++ script/script.js
@@ -35,38 +35,77 @@ $(document).ready(function () {
       }
     ]
   });
-});
 
-const customSelectors = $('.customSelect');
-const slideSpeed = 200;
+  const customSelectors = $('.customSelect');
+  const slideSpeed = 200;
 
-function closeAll(el) {
-  $.each(customSelectors, function () {
-    const ul = this.querySelector('ul');
-    const isVisible = $(ul).is(':visible');
-    const isPassedElement = el && el[0] === ul;
-    if (isVisible && !isPassedElement) {
-      $(ul).slideUp(slideSpeed);
-    }
-  });
-}
-
-$('html').on('click', function(e) {
-  if($(e.target).parents('.customSelect').length == 0) {
-    /* Hide dropdown here */
-    closeAll();
+  function closeAll(el) {
+    $.each(customSelectors, function () {
+      const ul = this.querySelector('ul');
+      const isVisible = $(ul).is(':visible');
+      const isPassedElement = el && el[0] === ul;
+      if (isVisible && !isPassedElement) {
+        $(ul).slideUp(slideSpeed);
+      }
+    });
   }
+
+  $('html').on('click', function(e) {
+    console.log(e.target);
+    if($(e.target).parents('.customSelect').length == 0) {
+      /* Hide dropdown here */
+      closeAll();
+    }
+    e.preventDefault();
+  });
+
+  $(customSelectors).on('click', function () {
+    const ul = this.querySelector('ul');
+    $(ul).slideToggle(slideSpeed);
+    closeAll($(ul));
+  });
+
+  $(customSelectors).on('click', 'li', function () {
+    const parent = $(this).parent().siblings('.selected').find('.selected');
+    parent[0].innerHTML = this.innerHTML;
+    closeAll($(this).parent());
+  });
+
+  $('#testButton').on('click', function() {
+    console.log('clicked ')
+  })
 });
 
-$(customSelectors).on('click', function () {
-  const ul = this.querySelector('ul');
-  $(ul).slideToggle(slideSpeed);
-  closeAll($(ul));
-});
-
-$(customSelectors).on('click', 'li', function () {
-  const parent = $(this).parent().siblings('.selected').find('.selected');
-  parent[0].innerHTML = this.innerHTML;
-  closeAll($(this).parent());
-});
+// const customSelectors = $('.customSelect');
+// const slideSpeed = 200;
+//
+// function closeAll(el) {
+//   $.each(customSelectors, function () {
+//     const ul = this.querySelector('ul');
+//     const isVisible = $(ul).is(':visible');
+//     const isPassedElement = el && el[0] === ul;
+//     if (isVisible && !isPassedElement) {
+//       $(ul).slideUp(slideSpeed);
+//     }
+//   });
+// }
+//
+// $('html').on('click', function(e) {
+//   if($(e.target).parents('.customSelect').length == 0) {
+//     /* Hide dropdown here */
+//     closeAll();
+//   }
+// });
+//
+// $(customSelectors).on('click', function () {
+//   const ul = this.querySelector('ul');
+//   $(ul).slideToggle(slideSpeed);
+//   closeAll($(ul));
+// });
+//
+// $(customSelectors).on('click', 'li', function () {
+//   const parent = $(this).parent().siblings('.selected').find('.selected');
+//   parent[0].innerHTML = this.innerHTML;
+//   closeAll($(this).parent());
+// });
 
